537ed280793bc916040faaa433555f8b1e78a444
yangxd <709820562@qq.com>
2018-06-26 00:02:36 +0800

    初始化product

---
+++ client/src/main/java/com/spring/yxd/product/client/ProductClient.java
@@ -0,0 +1,19 @@
+package com.spring.yxd.product.client;
+
+import com.spring.yxd.product.common.DecreaseStockInput;
+import com.spring.yxd.product.common.ProductInfoOutput;
+import org.springframework.cloud.netflix.feign.FeignClient;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+
+import java.util.List;
+
+@FeignClient(name = "product")
+public interface ProductClient {
+
+    @PostMapping("/product/listForOrder")
+    List<ProductInfoOutput> listForOrder(@RequestBody List<String> productIdList);
+
+    @PostMapping("/product/decreaseStock")
+    void decreaseStock(@RequestBody List<DecreaseStockInput> decreaseStockInputList);
+}
